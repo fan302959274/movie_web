f49d46c1591a4943c7335250c547a89712b985b9
sh00859 <302959274@qq.com>
2017-09-29 20:00:53 +0800

    集成rabbitmq

--- src/test/java/com/node/NodeApplicationTests.java
+++ src/test/java/com/node/NodeApplicationTests.java
@@ -1,17 +1,23 @@
 package com.node;
 
+import com.movie.VideoApplication;
+import com.movie.mq.Sender;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import redis.clients.jedis.Jedis;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes=NodeApplicationTests.class)
+@SpringBootTest(classes=VideoApplication.class)
 public class NodeApplicationTests {
+
 	protected final Logger logger = LogManager.getLogger(this.getClass());
+	@Autowired
+	private Sender sender;
 	@Test
 	public void contextLoads() {
 		Jedis jedis = new Jedis("192.168.20.8",6379);
@@ -24,6 +30,13 @@ public class NodeApplicationTests {
 		System.out.println(jedis.get("user").toString());
 		jedis.set("user","name");
 		System.out.println(jedis.get("user"));
+
+	}
+
+	@Test
+	public void activemq() {
+			sender.send();
+			System.out.println("1213213");
 
 	}
 
