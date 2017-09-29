719033c6e484d6924ad2466f6e707122682c858f
sh00859 <302959274@qq.com>
2017-09-29 15:04:28 +0800

    ehcache转换成redis缓存

--- src/test/java/com/node/NodeApplicationTests.java
+++ src/test/java/com/node/NodeApplicationTests.java
@@ -1,16 +1,30 @@
 package com.node;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes=NodeApplicationTests.class)
 public class NodeApplicationTests {
-
+	protected final Logger logger = LogManager.getLogger(this.getClass());
 	@Test
 	public void contextLoads() {
+		Jedis jedis = new Jedis("192.168.20.8",6379);
+//		jedis.auth("123");     //密码认证
+		System.out.println("Connection to server sucessfully");
+		//查看服务是否运行
+		logger.info(jedis.get("name"));
+		jedis.set("user","namess");
+		// System.out.println("Server is running: "+jedis.ping());
+		System.out.println(jedis.get("user").toString());
+		jedis.set("user","name");
+		System.out.println(jedis.get("user"));
+
 	}
 
 }
